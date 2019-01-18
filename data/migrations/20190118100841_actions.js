51f0f4ab9a928c7024240f15dcf3e083109e4345
BrandCam <BrandCam>
2019-01-18 10:49:50 -0600

    added migrations and set up knexfile.js still have to check for bugs and typos, also set up index.js to listen on port 3333

---
+++ data/migrations/20190118100841_actions.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("actions", tbl => {
+    tbl.increments();
+    tbl.string("descrtiption", 250).notNullable();
+    tbl.text("notes", 550).notNullable();
+    tbl.boolean("completed").defaultTo(false);
+    tbl.integer("project_id");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("actions");
+};
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const knex = require("knex");
+
+const knexConfig = require("./knexfile.js");
+
+const db = knex(knexConfig.development);
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(3333, () => console.log("\nWorking!!\n"));
