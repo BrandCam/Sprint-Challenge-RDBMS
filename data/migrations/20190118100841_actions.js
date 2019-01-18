0e265d4ef79f1f0cbccbad41cb7b63b14d13adbe
BrandCam <BrandCam>
2019-01-18 13:03:08 -0600

    fixed some typos, now the seeds are working

--- data/migrations/20190118100841_actions.js
+++ data/migrations/20190118100841_actions.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("actions", tbl => {
     tbl.increments();
-    tbl.string("descrtiption", 250).notNullable();
+    tbl.string("description", 250).notNullable();
     tbl.text("notes", 550).notNullable();
     tbl.boolean("completed").defaultTo(false);
     tbl.integer("project_id");
